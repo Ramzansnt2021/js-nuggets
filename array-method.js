fedbafdbf0a9a590c0fc7542a7237af19a8be0c4
Ramzan Raza <ramraza9190@sbtjapan.com>
2023-06-16 18:34:04 +0500

    array method concepts with new functions

--- array-method.js
+++ array-method.js
@@ -9,43 +9,109 @@ const food = [
   'guava',
 ];
 
+console.log('Original Array: ' + food);
+
 food.forEach((items) => {
   console.log(items);
 });
+console.log('Original Array: ' + food);
 
 const bananas = food.map((item) => {
-  console.log('Bananas: ');
+  console.log('Bananas: ' + item);
 });
+console.log('Original Array: ' + food);
 
 const duplicateFood = food.map((item) => item + item);
 console.log(duplicateFood);
+console.log('Original Array: ' + food);
 
 const fruits = food.filter((items) => items.length <= 5);
 console.log(fruits);
+console.log('Original Array: ' + food);
 
 // const looking_for_element = food.find((item) => item === 'eggs');
 // console.log(looking_for_element);
+// console.log('Original Array: ' + food);
 
 // const looking_for_element = food.findIndex((item) => item === 'eggs');
 // console.log(looking_for_element);
+// console.log('Original Array: ' + food);
 
 // const looking_for_element = food.indexOf('eggs');
 // console.log(looking_for_element);
+// console.log('Original Array: ' + food);
 
 // const looking_for_element = food.some((item) => item === 'eggs');
 // console.log(looking_for_element);
 
 // const looking_for_element = food.every((item) => item !== 'eggs');
 // console.log(looking_for_element);
+// console.log('Original Array: ' + food);
 
 // const looking_for_element = food.includes((item) => item === 'eggs');
 // console.log(looking_for_element);
+// console.log('Original Array: ' + food);
 
 const foodStr = food.toString();
 console.log(foodStr);
+console.log('Original Array: ' + food);
 
 const foodJoin = food.join(', ');
 console.log(foodJoin);
+console.log('Original Array: ' + food);
 
 const foodFill = food.fill('onion', 2, 5);
 console.log(foodFill);
+console.log('Original Array: ' + food);
+
+const foodCopy = food.copyWithin(2, 4, 6);
+console.log(foodCopy);
+console.log('Original Array: ' + food);
+
+const foodSlice = food.slice(1, 4);
+console.log(foodSlice);
+console.log('Original Array: ' + food);
+
+const foodSplice = food.splice(0, 2, 'eggs', 'milk');
+console.log(foodSplice);
+console.log('Original Array: ' + food);
+
+// Builtin array function  sort
+const foodSort = food.sort();
+console.log(foodSort);
+console.log('Original Array: ' + food);
+
+const numbers = [88, 9, 66, 5, 11, 6, 8, 2, 1];
+console.log('Original Array:', numbers);
+numbers.sort((a, b) => {
+  return a - b;
+});
+console.log(numbers);
+
+const num = numbers.reverse();
+console.log(num);
+
+const str = '11234';
+
+const numberFrom = Array.from(str, (item) => Number(item));
+console.log(numberFrom);
+
+let check_array = Array.isArray(food);
+console.log(check_array);
+check_array = Array.isArray(str);
+console.log(check_array);
+
+const new_array = numbers.valueOf();
+console.log('new_array: ', new_array);
+
+const foodEntries = food.entries();
+for (let elements of foodEntries) {
+  console.log(elements);
+}
+
+function reducer(previousValue, currentValue) {
+  return previousValue + currentValue;
+}
+
+const sum = numbers.reduce(reducer, 0);
+console.log(sum);
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 ## Advance JavaScript Topics
 
+JS Nuggets repo [nuggets](https://github.com/Ramzansnt2021/js-nuggets.git)
+
 ### JavaScript Nuggets
 
 Nuggets Following [Repo](https://github.com/john-smilga/javascript-nuggets)
